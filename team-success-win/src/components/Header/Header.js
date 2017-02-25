95ed8903441e1b696f67689ec5134bfc879f9af4
Kevin <kevinkuchar2378@gmail.com>
2017-02-25 12:53:06 -0500

    feat(lol): skeleton of react app

---
+++ team-success-win/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        Team Success Win
+      </div>
+    );
+  }
+}
+
+export default Header;
